5a71e0df00ce37b303b358209c65d7806d35fb2b
Max Le <gop_max@m1.com.sg>
2023-10-28 20:34:20 +1100

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for val in row:
-            print("{:d}".format(val), end=" ")
+            print("{:2}".format(val), end="")
         print("")
