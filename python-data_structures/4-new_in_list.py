0babc433558fcb861b44c0d533d6577e0091149f
Max Le <gop_max@m1.com.sg>
2023-10-28 20:22:56 +1100

    add copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    modified_list = my_list
+    modified_list = my_list.copy()
     if 0 <= idx < len(my_list):
         modified_list[idx] = element
         return modified_list
