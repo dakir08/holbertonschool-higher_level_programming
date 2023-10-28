20eeb0a1318f04441ee5916e87ddd5fcde3f2bb7
Max Le <gop_max@m1.com.sg>
2023-10-28 19:34:46 +1100

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 def print_arg(arg):
     n = len(arg) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
+    if n == 1:
+        print("{:d} argument:".format(n))
     else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, arg[i]))
-            i += 1
+        print("{:d} arguments:".format(n))
+    i = 1
+    while i <= n:
+        print("{:d}: {:s}".format(i, arg[i]))
+        i += 1
+
 
 if __name__ == "__main__":
     import sys
