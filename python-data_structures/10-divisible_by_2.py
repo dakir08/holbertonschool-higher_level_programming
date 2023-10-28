273bc0c019326b7a7e0289e3440966144caecf72
Max Le <gop_max@m1.com.sg>
2023-10-28 21:05:39 +1100

    10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list = my_list.copy()
+    for num, index in enumerate(my_list):
+        if num % 2 == 0:
+            list[index] = True
+        else:
+            list[index] = False
+    return list
