8128f56d68ac333129b92c367a28f562a234b334
Max Le <gop_max@m1.com.sg>
2023-11-11 23:23:39 +1100

    update

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,75 +1,84 @@
 #!/usr/bin/python3
 """
-Rectangle class
+Module Rectangle
 """
 
 
 class Rectangle:
+    """Rectangle class"""
     number_of_instances = 0
 
-    """
-    Constructor
-    """
+
     def __init__(self, width=0, height=0):
+        """
+        Constructor
+        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances +=1 
 
-    """
-    Get width
-    """
+
     @property
     def width(self):
+        """
+        Get width
+        """
         return self.__width
 
-    """
-    Set width
-    """
+
     @width.setter
     def width(self, value):
+        """
+        Set width
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """
-    Get height
-    """
+
     @property
     def height(self):
+        """
+        Get height
+        """
         """Get/set the height of the rectangle."""
         return self.__height
 
-    """
-    Set width
-    """
+
     @height.setter
     def height(self, value):
+        """
+        Set width
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    """
-    Calculate area of Rectangle.
-    """
+
     def area(self):
+        """
+        Calculate area of Rectangle.
+        """
         return self.__height * self.__width
 
-    """
-    Calculate perimeter of Rectangle object.
-    """
+
     def perimeter(self):
+        """
+        Calculate perimeter of Rectangle object.
+        """
         if self.__height == 0 or self.__width == 0:
             return 0
         return (self.__height + self.__width) * 2
 
-    """
-    Return rectangle with #
-    """
+
     def __str__(self):
+        """
+        Return rectangle with #
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
         rect = []
@@ -80,16 +89,18 @@ class Rectangle:
                 rect.append("\n")
         return "".join(rect)
 
-    """
-    Return represenation of the rectangle
-    """
+
     def __repr__(self):
+        """
+        Return represenation of the rectangle
+        """
         rect = f"Rectangle({self.__width}, {self.height})"
         return rect
 
-    """
-    Delete rectangle
-    """
+
     def __del__(self):
+        """
+        Delete rectangle
+        """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
