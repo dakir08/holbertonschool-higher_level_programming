26e9e662793d13237b5716ae0f0683155681a41e
Max Le <gop_max@m1.com.sg>
2023-11-11 23:06:59 +1100

    3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -69,12 +69,9 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ("")
-
         rect = []
         for i in range(self.__height):
             for _ in range(self.__width):
                 rect.append("#")
             rect.append("\n")
-
-        print(rect)
         return "".join(rect)
