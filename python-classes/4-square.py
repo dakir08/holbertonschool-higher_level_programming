0dd2971ae39d75fc75d46b48b9517f9ea423ed37
Max Le <gop_max@m1.com.sg>
2023-11-11 19:42:41 +1100

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a Square class"""
+
+
+class Square:
+    """Constructor"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size mustbe >= 0")
+        self.__size = size
+
+    """Return squared area"""
+    def area(self):
+        return self.__size ** 2
+
+    """Get current size"""
+    @property
+    def size(self):
+        return self.__size
+    
+    """Set current size"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size mustbe >= 0")
+        self.__size = value
