8cbabf9a98e3eb2a8a68a055d0603b68985f619f
Max Le <gop_max@m1.com.sg>
2023-11-21 20:03:06 +1100

    add documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Function that return the list 
+of available attributes and methods of an object
+"""
 def lookup(obj):
+    """
+    the return the list of available attributes and methods of an object
+    """
     return dir(obj)
