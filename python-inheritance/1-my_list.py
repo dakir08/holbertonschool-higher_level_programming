bebe44d97be4397492422e98a1b35bafdb54fc3e
Max Le <gop_max@m1.com.sg>
2023-11-21 20:10:01 +1100

    1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' 
+1-my_list.py
+'''
+
+
+class MyList(list):
+    ''' 
+    MyList class
+    '''
+
+    def print_sorted(self):
+        '''
+        print the sorted list
+        '''
+        print(sorted(self))
