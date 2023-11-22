0063f840a836ec9462f1aededa241625a56970c5
Max Le <gop_max@m1.com.sg>
2023-11-22 19:05:33 +1100

    3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+3-is_kind_of_class.py
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if object is an instance of the class or interited class
+    """
+    return isinstance(obj, a_class)
