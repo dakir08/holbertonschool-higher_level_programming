0bfde1fe33908122978d2695edc37e7e276db7e4
Max Le <gop_max@m1.com.sg>
2023-11-22 20:11:23 +1100

    update 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,5 +13,6 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self._size = size
-        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size ,size)
