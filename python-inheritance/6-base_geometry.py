67eb53a03dc9c4eaa2890e282852daa05a4ec81a
Max Le <gop_max@m1.com.sg>
2023-11-22 19:13:21 +1100

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module.
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    
+    def area(self):
+        """
+        Implementation later
+        """
+        raise Exception("area() is not implemented")
