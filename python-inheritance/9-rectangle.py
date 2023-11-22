6fb5e6f32cdec29d7926b0636e3fd89434e87716
Max Le <gop_max@m1.com.sg>
2023-11-22 20:01:48 +1100

    9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Rectangle module.
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        return area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ 
+        print friendly version of rectangle
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
