0b26b4573fb2e29a590d5a55ee7847816b765189
Max Le <gop_max@m1.com.sg>
2023-11-22 20:12:01 +1100

    fix spacing

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -15,4 +15,4 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size ,size)
+        super().__init__(size, size)
