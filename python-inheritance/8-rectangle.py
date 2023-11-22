c414b8103e20a78a4c282aa954a08b6501ceef29
Max Le <gop_max@m1.com.sg>
2023-11-22 19:23:38 +1100

    8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+Rectangle module.
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
