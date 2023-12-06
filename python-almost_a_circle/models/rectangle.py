c198f1224ab4c9be99be7b1159fb362e9dd291c6
Max Le <gop_max@m1.com.sg>
2023-12-06 17:51:20 +1100

    4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -34,6 +34,10 @@ class Rectangle(Base):
         """
         Set rectangle width.
         """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -48,6 +52,10 @@ class Rectangle(Base):
         """
         Set rectangle height.
         """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -62,18 +70,26 @@ class Rectangle(Base):
         """
         Set x coordinator of the rectangle.
         """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
         """
         Get y coordinator of the rectangle.
-        """
+        """        
         return self.__y
 
     @y.setter
     def y(self, value):
         """
         Set y coordinator of the rectangle.
-        """
+        """  
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
