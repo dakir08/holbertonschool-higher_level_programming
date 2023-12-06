dff404a2c828181810818330e9951e24cb6c5fac
Max Le <gop_max@m1.com.sg>
2023-12-06 17:47:11 +1100

    add getter and setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,3 +21,59 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """
+        Get rectangle width.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set rectangle width.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Get rectangle height.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set rectangle height.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Get x coordinator of the rectangle.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set x coordinator of the rectangle.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Get y coordinator of the rectangle.
+        """        
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set y coordinator of the rectangle.
+        """  
+        self.__y = value
