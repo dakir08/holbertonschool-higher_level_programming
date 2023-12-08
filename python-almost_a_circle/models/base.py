630602a700e4e735991fe2bd7e98745b5b6406d1
Max Le <gop_max@m1.com.sg>
2023-12-08 11:19:05 +1100

    16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,11 +23,30 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts.
-
-        Args:
-            list_dictionaries (list): A list of dictionaries.
+        """
+        Return the JSON serialization of a list of dicts.
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON serialization of a list of objects to a file.
+        """
+        # Define the filename based on the class name
+        filename = f"{cls.__name__}.json"
+
+        # Open the file for writing
+        with open(filename, "w") as jsonfile:
+            # Check if the list of objects is empty or None
+            if not list_objs:
+                # Write an empty JSON array to the file
+                jsonfile.write("[]")
+            else:
+                # Convert each object in the list to its dictionary representation
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+
+                # Serialize the list of dictionaries to a JSON string and write to the file
+                jsonfile.write(Base.to_json_string(list_dicts))
