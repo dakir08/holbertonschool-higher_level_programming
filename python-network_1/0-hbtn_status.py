3e7564e5d1ddd8a756401b2b757852db62c32092
Max Le <gop_max@m1.com.sg>
2024-01-31 10:24:09 +0700

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,11 +3,13 @@
 script that fetches https://intranet.hbtn.io/status
 """
 
-import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-    content = res.read()
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf-8')}")
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
