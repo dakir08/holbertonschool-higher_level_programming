3e2cf4dc8a5e160daa77d6de0f460253242449be
Max Le <gop_max@m1.com.sg>
2024-01-31 10:17:20 +0700

    update document

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import urllib.request
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
     content = res.read()
