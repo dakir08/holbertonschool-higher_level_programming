15318a7ff1668aad3836d8c04981111789289c70
Max Le <gop_max@m1.com.sg>
2024-01-31 10:20:34 +0700

    revert

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@ script that fetches https://intranet.hbtn.io/status
 
 import urllib.request
 
-# with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-#     content = res.read()
-#     print("Body response:")
-#     print(f"\t- type: {type(content)}")
-#     print(f"\t- content: {content}")
-#     print(f"\t- utf8 content: {content.decode('utf-8')}")
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    content = res.read()
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
