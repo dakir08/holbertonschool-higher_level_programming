59c6d74fb5599bd6f16baaf8b18099b08c7138b9
Max Le <gop_max@m1.com.sg>
2024-02-01 09:39:03 +0700

    6

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    body = {
+        "email": sys.argv[2]
+    }
+    r = requests.post(url, data=body)
+    print(r.text)
