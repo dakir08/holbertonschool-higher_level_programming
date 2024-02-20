37cad423b9b7d1a42df85271e2459588a354c820
Max Le <gop_max@m1.com.sg>
2024-02-20 16:32:37 +1100

    update

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in the table second_table
-select score, COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
