e9ee960f1d1a5a57911a56cc0453fb6199f4e283
Max Le <gop_max@m1.com.sg>
2024-02-27 17:05:59 +1100

    update format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute(f"""SELECT * FROM states WHERE name
-                LIKE BINARY {sys.argv[4]} ORDER BY states.id""")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY '{}' ORDER BY states.id""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
