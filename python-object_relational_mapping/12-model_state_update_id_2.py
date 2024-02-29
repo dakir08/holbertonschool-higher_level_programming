d5ac75a8c058126edcb321ecf890b8152a0c571c
Max Le <gop_max@m1.com.sg>
2024-02-29 16:13:53 +1100

    12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+adds the State object “Louisiana” to the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    query = session.query(State).filter_by(id=2).first()
+    query.name = "New Mexico"
+    session.commit()
