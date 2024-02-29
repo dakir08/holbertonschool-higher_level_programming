5342ff474f5121cfa83528006c0439816b9ec2fe
Max Le <gop_max@m1.com.sg>
2024-02-29 16:27:36 +1100

    14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+print city
+"""
+import sys
+from model_state import State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for result in (session.query(State.name, City.id, City.name)
+            .filter(State.id == City.state_id)):
+        print(f"{result[0]}: ({result[1]}) {result[2]}")
