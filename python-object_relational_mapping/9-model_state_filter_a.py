5a4afbfab968e5d6032679759327b83454c442f5
Max Le <gop_max@m1.com.sg>
2024-02-29 15:57:33 +1100

    update validation

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,5 +15,6 @@ if __name__ == "__main__":
         sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
+    for state in session.query(State).filter(
+        State.name.like("%a%")).order_by(State.id):
         print(state.id, state.name, sep=": ")
