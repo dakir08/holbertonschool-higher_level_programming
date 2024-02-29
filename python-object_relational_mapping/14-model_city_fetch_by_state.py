638d4f81d255d168886fedf9a9819a3a27eee595
Max Le <gop_max@m1.com.sg>
2024-02-29 16:29:37 +1100

    validation

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -17,5 +17,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for result in session.query(State.name,
-                                City.id, City.name).filter(State.id == City.state_id):
+                                City.id, City.name).filter(
+                                    State.id == City.state_id):
         print(f"{result[0]}: ({result[1]}) {result[2]}")
